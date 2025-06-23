1572711501ab1ac2f44812f24c09e67c27663f42
Ruchi Sharma <112816398+snowXshade@users.noreply.github.com>
2025-06-23 14:30:54 +0530

    little js update

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// no update on js yet
+var list = document.getElementById("list");
+var ul = document.getElementById("ul");
+var input = document.getElementById("task-input");
+var btn = document.getElementById("btn");
+
+function addTask() {
+inputValue = input.value;
+// console.log(inputValue);
+
+var li = document.createElement("li");
+li.textContent = inputValue;
+
+ul.appendChild(li);
+
+input.value="";
+}
